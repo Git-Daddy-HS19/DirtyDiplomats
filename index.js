6f0cab005d20dcc1c50860dc6f19c965cb4141ba
eniallator <eniallatormc@gmail.com>
2019-11-09 15:27:22 +0000

    Added a runningGames object

--- index.js
+++ index.js
@@ -36,5 +36,22 @@ const genUniqueID = (length = 4) => {
     return id
 }
 
-app.use(express.static('public'))
-app.listen(port, () => console.log(`STATUS: Web server active on port ${port}!`))
+const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const genUniqueID = (length = 4) => {
+    let id = ''
+    for (let i = 0; i < length; i++) {
+        id += charSet[Math.floor(Math.random() * charSet.length)]
+    }
+    return id
+}
+
+const runningGames = {}
+
+app.post('/create-game', (req, res) => {
+    let newGameID
+    while (runningGames[(newGameID = genUniqueID())] === undefined) {}
+    runningGames[newGameID] = {}
+    res.json({ id: newGameID })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
